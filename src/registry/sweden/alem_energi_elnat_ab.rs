5560bc65c095847aaf1278607c01129d61e2f111
Frida Eriksson <frida@spotpilot.io>
2025-09-09 14:24:40 +0200

    Add basic fee info for alem energi

--- src/registry/sweden/alem_energi_elnat_ab.rs
+++ src/registry/sweden/alem_energi_elnat_ab.rs
@@ -6,17 +6,29 @@ pub const ALEM_ENERGI_ELNAT_AB: GridOperator = GridOperator::builder()
     .name("Ålem Energi Elnät AB")
     .vat_number("SE556179650801")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(16, 200))
     .links(Links::new(
         Link::builder(FEE_LINK).content_locator_default().build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(458, 0)),
+            (20, Money::new(779, 0)),
+            (25, Money::new(1012, 0)),
+            (35, Money::new(1507, 0)),
+            (50, Money::new(2285, 0)),
+            (63, Money::new(2993, 0)),
+            (80, Money::new(3721, 0)),
+            (100, Money::new(4764, 0)),
+            (125, Money::new(6116, 0)),
+            (160, Money::new(8310, 0)),
+            (200, Money::new(11179, 0)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(27.65))
         .other_fees(OtherFees::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
