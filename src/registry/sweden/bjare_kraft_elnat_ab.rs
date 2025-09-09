0675f074b02fab68e450b6eafe3436a2a680d87c
Frida Eriksson <frida@spotpilot.io>
2025-09-09 15:05:18 +0200

    Add basic fee info for bjare kraft

--- src/registry/sweden/bjare_kraft_elnat_ab.rs
+++ src/registry/sweden/bjare_kraft_elnat_ab.rs
@@ -6,19 +6,31 @@ pub const BJARE_KRAFT_ELNAT_AB: GridOperator = GridOperator::builder()
     .name("Bjäre Kraft Elnät AB")
     .vat_number("SE559427724501")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(16, 200))
     .links(Links::new(
         Link::builder(FEE_LINK)
             .plain_content_locator(".news-article")
             .build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 2, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(355, 0)),
+            (20, Money::new(565, 0)),
+            (25, Money::new(565, 0)),
+            (35, Money::new(1295, 0)),
+            (50, Money::new(1984, 0)),
+            (63, Money::new(2761, 0)),
+            (80, Money::new(3584, 0)),
+            (100, Money::new(4703, 0)),
+            (125, Money::new(6096, 0)),
+            (160, Money::new(8029, 0)),
+            (200, Money::new(9866, 0)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(19.3))
         .other_fees(OtherFees::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
