02c7265c8d9cf89b6bb61e5067317a5a6a49200a
Frida Eriksson <frida@spotpilot.io>
2025-09-11 11:22:14 +0200

    Add basic fee info for Gavle energi

--- src/registry/sweden/gavle_energi_elnat_ab.rs
+++ src/registry/sweden/gavle_energi_elnat_ab.rs
@@ -6,17 +6,23 @@ pub const GAVLE_ENERGI_ELNAT_AB: GridOperator = GridOperator::builder()
     .name("Gävle Energi Elnät AB")
     .vat_number("SE559397122801")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(16, 50))
     .links(Links::new(
         Link::builder(FEE_LINK).content_locator_default().build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
+        .from_date(2025, 1, 1)
         .monthly_fee(Cost::Unverified)
-        .monthly_production_fee(Cost::Unverified)
+        .monthly_production_fee(Cost::fuses(&[
+            (16, Money::new(4150, 0).divide_by(12)),
+            (20, Money::new(6145, 0).divide_by(12)),
+            (25, Money::new(8260, 0).divide_by(12)),
+            (35, Money::new(10200, 0).divide_by(12)),
+            (50, Money::new(15090, 0).divide_by(12)),
+        ]))
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(15.0))
         .other_fees(OtherFees::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
