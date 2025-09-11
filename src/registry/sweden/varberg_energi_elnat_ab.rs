88594137d0f9004be08758391bf2e6ba4d157e2f
Gosia Stupak <stupak.malgorzata@gmail.com>
2025-09-11 21:54:33 +0200

    fix: Power tariffs and simplify monthly fee

--- src/registry/sweden/varberg_energi_elnat_ab.rs
+++ src/registry/sweden/varberg_energi_elnat_ab.rs
@@ -15,34 +15,28 @@ pub const VARBERG_ENERGI_ELNAT_AB: GridOperator = GridOperator::builder()
     .price_lists(&[PriceList::builder()
         .from_date(2025, 9, 1)
         .monthly_fee(Cost::fuses(&[
-            (16, Money::new(2428, 00).add_vat(Country::SE).divide_by(12)),
-            (20, Money::new(3380, 00).add_vat(Country::SE).divide_by(12)),
-            (25, Money::new(5988, 00).add_vat(Country::SE).divide_by(12)),
-            (35, Money::new(9760, 00).add_vat(Country::SE).divide_by(12)),
-            (50, Money::new(13048, 00).add_vat(Country::SE).divide_by(12)),
-            (63, Money::new(17540, 00).add_vat(Country::SE).divide_by(12)),
-            (80, Money::new(22936, 00).add_vat(Country::SE).divide_by(12)),
-            (
-                100,
-                Money::new(28544, 00).add_vat(Country::SE).divide_by(12),
-            ),
-            (
-                125,
-                Money::new(34924, 00).add_vat(Country::SE).divide_by(12),
-            ),
-            (
-                160,
-                Money::new(45840, 00).add_vat(Country::SE).divide_by(12),
-            ),
-            (
-                200,
-                Money::new(55692, 00).add_vat(Country::SE).divide_by(12),
-            ),
+            (16, Money::new(3035, 00).divide_by(12)),
+            (20, Money::new(4225, 00).divide_by(12)),
+            (25, Money::new(7485, 00).divide_by(12)),
+            (35, Money::new(12200, 00).divide_by(12)),
+            (50, Money::new(16310, 00).divide_by(12)),
+            (63, Money::new(21925, 00).divide_by(12)),
+            (80, Money::new(28670, 00).divide_by(12)),
+            (100, Money::new(35680, 00).divide_by(12)),
+            (125, Money::new(43655, 00).divide_by(12)),
+            (160, Money::new(57300, 00).divide_by(12)),
+            (200, Money::new(69615, 00).divide_by(12)),
         ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::fixed_subunit(9.5)) //OBS räknat pris inkl moms manuellt (7,6*1,25)
+        .transfer_fee(TransferFee::fixed_subunit(9.5))
         .other_fees(OtherFees::Unverified)
-        .power_tariff(PowerTariff::NotImplemented)
+        .power_tariff(PowerTariff::new(
+            TariffCalculationMethod::AverageHours((3)),
+            CostPeriods::new(&[CostPeriod::builder()
+                .load(Base)
+                .cost(Cost::fixed(30, 00))
+                .build()]),
+        ))
         .build()])
     .build();
