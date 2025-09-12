8538a60ab308794ce5f4f7bd78da95e876d883ba
FridaSpotpilot <frida@spotpilot.io>
2025-09-12 14:12:01 +0200

    feat: Add basic fee info for boo energi (#29)

--- src/registry/sweden/boo_energi_ek.rs
+++ src/registry/sweden/boo_energi_ek.rs
@@ -11,12 +11,19 @@ pub const BOO_ENERGI_EK: GridOperator = GridOperator::builder()
             .build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(333, 0)),
+            (20, Money::new(379, 0)),
+            (25, Money::new(440, 0)),
+            (35, Money::new(580, 0)),
+            (50, Money::new(794, 0)),
+            (63, Money::new(980, 0)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(33.00))
         .other_fees(OtherFees::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
