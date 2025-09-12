781967ee59840e39cd8266be9db6c4cc56f6fbe8
gosiastupak <stupak.malgorzata@gmail.com>
2025-09-12 14:36:51 +0200

    Varbergsortens Elkraft (#44)
    * Varbergsortens Elkraft
    
    * fix: Power tariff

--- src/registry/sweden/varbergsortens_elnat_ab.rs
+++ src/registry/sweden/varbergsortens_elnat_ab.rs
@@ -6,19 +6,25 @@ pub const VARBERGSORTENS_ELNAT_AB: GridOperator = GridOperator::builder()
     .name("Varbergsortens Elnät AB")
     .vat_number("SE559481776801")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(20, 63))
     .links(Links::new(
         Link::builder(FEE_LINK)
             .plain_content_locator("#content")
             .build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 4, 1)
+        .monthly_fee(Cost::fuses(&[
+            (20, Money::new(5063, 0).divide_by(12)),
+            (25, Money::new(7163, 0).divide_by(12)),
+            (35, Money::new(10625, 0).divide_by(12)),
+            (50, Money::new(16188, 0).divide_by(12)),
+            (63, Money::new(20625, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(20.0))
         .other_fees(OtherFees::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
