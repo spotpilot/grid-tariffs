c8f74da9c36032a2e8804cd2652632c7beeb62d8
FridaSpotpilot <frida@spotpilot.io>
2025-09-12 14:19:36 +0200

    Add basic fee info for Borgholms Energi (#38)

--- src/registry/sweden/borgholm_energi_elnat_ab.rs
+++ src/registry/sweden/borgholm_energi_elnat_ab.rs
@@ -6,17 +6,25 @@ pub const BORGHOLM_ENERGI_ELNAT_AB: GridOperator = GridOperator::builder()
     .name("Borgholm Energi Elnät AB")
     .vat_number("SE556020762201")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(16, 80))
     .links(Links::new(
         Link::builder(FEE_LINK).content_locator_default().build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(4678, 0).divide_by(12)),
+            (20, Money::new(6976, 0).divide_by(12)),
+            (25, Money::new(9023, 0).divide_by(12)),
+            (35, Money::new(13067, 0).divide_by(12)),
+            (50, Money::new(19318, 0).divide_by(12)),
+            (63, Money::new(25190, 0).divide_by(12)),
+            (80, Money::new(37261, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(26.1))
         .other_fees(OtherFees::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
