562e11e8b0f789ee2c85fd927ecc7a1c15cab2ca
FridaSpotpilot <frida@spotpilot.io>
2025-09-12 14:16:57 +0200

    Add basic fee info for Brittedals elnat (#34)
    * Add basic fee info for Brittedals elnat
    
    * fix: Set correct date
    
    ---------
    
    Co-authored-by: Jacob Magnusson <m@jacobian.se>

--- src/registry/sweden/brittedals_elnat_ek_for.rs
+++ src/registry/sweden/brittedals_elnat_ek_for.rs
@@ -11,12 +11,23 @@ pub const BRITTEDALS_ELNAT_EK_FOR: GridOperator = GridOperator::builder()
         Link::builder(FEE_LINK).content_locator_default().build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(580, 0)),
+            (20, Money::new(810, 0)),
+            (25, Money::new(1054, 0)),
+            (35, Money::new(1578, 0)),
+            (50, Money::new(2333, 0)),
+            (63, Money::new(2878, 0)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
+        .transfer_fee(TransferFee::SpotPriceVariable {
+            base_cost: Cost::fixed_subunit(14.0),
+            spot_price_multiplier: 0.09,
+            approximated: false,
+        })
         .other_fees(OtherFees::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
