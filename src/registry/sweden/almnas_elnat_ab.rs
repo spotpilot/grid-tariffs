923eb67095d0b2b276e0df40e19acafd6abc6296
Frida Eriksson <frida@spotpilot.io>
2025-09-15 15:42:57 +0200

    Add basic fee info for Almnas elnat

--- src/registry/sweden/almnas_elnat_ab.rs
+++ src/registry/sweden/almnas_elnat_ab.rs
@@ -6,16 +6,28 @@ pub static ALMNAS_ELNAT_AB: GridOperator = GridOperator::builder()
     .name("Almnäs Elnät AB")
     .vat_number("SE559139372201")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(16, 160))
     .links(Links::new(
         Link::builder(FEE_LINK).content_locator_default().build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2023, 3, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(7565, 0).divide_by(12)),
+            (20, Money::new(11149, 0).divide_by(12)),
+            (25, Money::new(13997, 0).divide_by(12)),
+            (35, Money::new(18458, 0).divide_by(12)),
+            (50, Money::new(25875, 0).divide_by(12)),
+            (63, Money::new(33567, 0).divide_by(12)),
+            (80, Money::new(43857, 0).divide_by(12)),
+            (100, Money::new(54159, 0).divide_by(12)),
+            (125, Money::new(62310, 0).divide_by(12)),
+            (130, Money::new(64085, 0).divide_by(12)),
+            (160, Money::new(79059, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(28.43))
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
