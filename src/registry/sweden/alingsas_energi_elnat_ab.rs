8b07c2ee21744a4263df840afdbcf126f9688d5b
Jacob Magnusson <m@jacobian.se>
2025-09-15 21:25:42 +0200

    feat(SE Alingsås): Add basic info

--- src/registry/sweden/alingsas_energi_elnat_ab.rs
+++ src/registry/sweden/alingsas_energi_elnat_ab.rs
@@ -1,21 +1,26 @@
 use crate::registry::prelude::*;
 
-const FEE_LINK: &str = "https://www.alingsasenergi.se/elnatsavgift-och-avtalsvillkor/";
+const FEE_LINK: &str = "https://www.alingsasenergi.se/elnatsavgift/";
 
 pub static ALINGSAS_ENERGI_ELNAT_AB: GridOperator = GridOperator::builder()
     .name("Alingsås Energi Elnät AB")
     .vat_number("SE559434718801")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(25, 63))
     .links(Links::new(
         Link::builder(FEE_LINK).content_locator_default().build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (25, Money::new(4598, 0).divide_by(12)),
+            (35, Money::new(10354, 0).divide_by(12)),
+            (50, Money::new(16500, 0).divide_by(12)),
+            (63, Money::new(24049, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
-        .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .feed_in_revenue(FeedInRevenue::fixed_subunit(4.50))
+        .transfer_fee(TransferFee::fixed_subunit(23.80))
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
