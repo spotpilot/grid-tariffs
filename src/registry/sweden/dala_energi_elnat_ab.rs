2112b3fa2f39bf85a4c8aef6bc9dce9fdfefc234
Frida Eriksson <frida@spotpilot.io>
2025-09-16 09:37:44 +0200

    feat(SE Dala): Add basic info

--- src/registry/sweden/dala_energi_elnat_ab.rs
+++ src/registry/sweden/dala_energi_elnat_ab.rs
@@ -11,11 +11,37 @@ pub static DALA_ENERGI_ELNAT_AB: GridOperator = GridOperator::builder()
             .build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 4, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(4100, 0).divide_by(12)),
+            (20, Money::new(5750, 0).divide_by(12)),
+            (25, Money::new(7200, 0).divide_by(12)),
+            (35, Money::new(9950, 0).divide_by(12)),
+            (50, Money::new(14250, 0).divide_by(12)),
+            (63, Money::new(18000, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(8.0))
+        .power_tariff(PowerTariff::new(
+            TariffCalculationMethod::AverageHours(3),
+            CostPeriods::new(&[
+                CostPeriod::builder()
+                    .load(High)
+                    .months(November, March)
+                    .hours(7, 19)
+                    .exclude_weekends_and_swedish_holidays()
+                    .fixed_cost(95, 0)
+                    .build(),
+                CostPeriod::builder()
+                    .load(High)
+                    .months(April, October)
+                    .hours(7, 19)
+                    .exclude_weekends_and_swedish_holidays()
+                    .fixed_cost(35, 0)
+                    .build(),
+                CostPeriod::builder().load(Low).fixed_cost(35, 0).build(),
+            ]),
+        ))
         .build()])
     .build();
