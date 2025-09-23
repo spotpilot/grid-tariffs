b3a300d3bc64ddbc30c682d399d69b29eae9f6ba
Frida Eriksson <frida@spotpilot.io>
2025-09-23 15:59:14 +0200

    feat(SE Uddevalla): Add basic info

--- src/registry/sweden/uddevalla_energi_elnat_ab.rs
+++ src/registry/sweden/uddevalla_energi_elnat_ab.rs
@@ -11,11 +11,18 @@ pub static UDDEVALLA_ENERGI_ELNAT_AB: GridOperator = GridOperator::builder()
             .build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(3750, 0).divide_by(12)),
+            (20, Money::new(5675, 0).divide_by(12)),
+            (25, Money::new(8000, 0).divide_by(12)),
+            (35, Money::new(11500, 0).divide_by(12)),
+            (50, Money::new(16250, 0).divide_by(12)),
+            (63, Money::new(21250, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(54.875))
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
