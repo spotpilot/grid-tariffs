ddb5361dd6fa7afe145e9a82b864dd7a5ea7618b
Frida Eriksson <frida@spotpilot.io>
2025-09-23 12:09:32 +0200

    feat(SE Skyllberg): Add basic info

--- src/registry/sweden/skyllbergs_elnat_ab.rs
+++ src/registry/sweden/skyllbergs_elnat_ab.rs
@@ -6,18 +6,30 @@ pub static SKYLLBERGS_ELNAT_AB: GridOperator = GridOperator::builder()
     .name("Skyllbergs Elnät AB")
     .vat_number("SE559440600001")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(16, 200))
     .links(Links::new(
         Link::builder(FEE_LINK)
             .plain_content_locator("[id='1230953390']")
             .build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(8570, 0).divide_by(12)),
+            (20, Money::new(10819, 0).divide_by(12)),
+            (25, Money::new(14623, 0).divide_by(12)),
+            (35, Money::new(20548, 0).divide_by(12)),
+            (50, Money::new(29193, 0).divide_by(12)),
+            (63, Money::new(37022, 0).divide_by(12)),
+            (80, Money::new(46926, 0).divide_by(12)),
+            (100, Money::new(58603, 0).divide_by(12)),
+            (125, Money::new(73236, 0).divide_by(12)),
+            (160, Money::new(95407, 0).divide_by(12)),
+            (200, Money::new(114926, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(45.0))
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
