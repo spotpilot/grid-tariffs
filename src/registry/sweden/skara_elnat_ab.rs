8f759a2412d206f9c404771d35502f74d8e9e855
Frida Eriksson <frida@spotpilot.io>
2025-09-23 10:56:46 +0200

    feat(SE Skara): Add basic info

--- src/registry/sweden/skara_elnat_ab.rs
+++ src/registry/sweden/skara_elnat_ab.rs
@@ -6,16 +6,28 @@ pub static SKARA_ELNAT_AB: GridOperator = GridOperator::builder()
     .name("Skara Elnät AB")
     .vat_number("SE559441591001")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(16, 200))
     .links(Links::new(
         Link::builder(FEE_LINK).content_locator_default().build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(2634, 0).divide_by(12)),
+            (20, Money::new(4367, 0).divide_by(12)),
+            (25, Money::new(5459, 0).divide_by(12)),
+            (35, Money::new(7632, 0).divide_by(12)),
+            (50, Money::new(10907, 0).divide_by(12)),
+            (63, Money::new(13891, 0).divide_by(12)),
+            (80, Money::new(18130, 0).divide_by(12)),
+            (100, Money::new(22037, 0).divide_by(12)),
+            (125, Money::new(27560, 0).divide_by(12)),
+            (160, Money::new(35277, 0).divide_by(12)),
+            (200, Money::new(43979, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(22.50))
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
