23fd189c0fdbfb3e9f97f3264f2b4428924e08cc
FridaSpotpilot <frida@spotpilot.io>
2025-10-02 20:14:10 +0200

    feat(SE Halmstad): Add basic info (#70)
    * feat(SE Halmstad): Add basic info
    
    * fix(SE Halmstad): Wrong transfer fee
    
    ---------
    
    Co-authored-by: Jacob Magnusson <m@jacobian.se>

--- src/registry/sweden/halmstads_energi_och_miljo_nat_ab.rs
+++ src/registry/sweden/halmstads_energi_och_miljo_nat_ab.rs
@@ -11,11 +11,18 @@ pub static HALMSTADS_ENERGI_OCH_MILJO_NAT_AB: GridOperator = GridOperator::build
             .build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(4290, 0).divide_by(12)),
+            (20, Money::new(5591, 0).divide_by(12)),
+            (25, Money::new(7454, 0).divide_by(12)),
+            (35, Money::new(11676, 0).divide_by(12)),
+            (50, Money::new(17592, 0).divide_by(12)),
+            (63, Money::new(22767, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(9.0))
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
