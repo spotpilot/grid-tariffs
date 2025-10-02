91d449f3ae1b94beb00ca02d7498fd8493a6cc57
FridaSpotpilot <frida@spotpilot.io>
2025-10-02 21:04:05 +0200

    feat(SE Soderhamn): Add basic info (#116)

--- src/registry/sweden/soderhamn_elnat_ab.rs
+++ src/registry/sweden/soderhamn_elnat_ab.rs
@@ -6,18 +6,29 @@ pub static SODERHAMN_ELNAT_AB: GridOperator = GridOperator::builder()
     .name("Söderhamn Elnät AB")
     .vat_number("SE556205999701")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(16, 160))
     .links(Links::new(
         Link::builder(FEE_LINK)
             .plain_content_locator(".pagecontent")
             .build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(3663, 0).divide_by(12)),
+            (20, Money::new(4800, 0).divide_by(12)),
+            (25, Money::new(6060, 0).divide_by(12)),
+            (35, Money::new(8720, 0).divide_by(12)),
+            (50, Money::new(12815, 0).divide_by(12)),
+            (63, Money::new(16166, 0).divide_by(12)),
+            (80, Money::new(21155, 0).divide_by(12)),
+            (100, Money::new(27241, 0).divide_by(12)),
+            (125, Money::new(35056, 0).divide_by(12)),
+            (160, Money::new(46168, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(20.3))
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
