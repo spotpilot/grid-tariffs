1252f4544a034a394f591ff00df8dd7f5f5012bc
FridaSpotpilot <frida@spotpilot.io>
2025-10-02 20:18:03 +0200

    feat(SE Kungalv): Add basic info (#88)
    * feat(SE Kungalv): Add basic info
    
    * fix(SE Kungälv): Wrong transfer fee
    
    ---------
    
    Co-authored-by: Jacob Magnusson <m@jacobian.se>

--- src/registry/sweden/kungalv_energi_ab.rs
+++ src/registry/sweden/kungalv_energi_ab.rs
@@ -11,11 +11,18 @@ pub static KUNGALV_ENERGI_AB: GridOperator = GridOperator::builder()
             .build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
+        .from_date(2025, 1, 1)
         .monthly_fee(Cost::Unverified)
-        .monthly_production_fee(Cost::Unverified)
+        .monthly_production_fee(Cost::fuses(&[
+            (16, Money::new(4104, 0).divide_by(12)),
+            (20, Money::new(4729, 0).divide_by(12)),
+            (25, Money::new(6011, 0).divide_by(12)),
+            (35, Money::new(8154, 0).divide_by(12)),
+            (50, Money::new(11521, 0).divide_by(12)),
+            (63, Money::new(14459, 0).divide_by(12)),
+        ]))
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(24.625))
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
