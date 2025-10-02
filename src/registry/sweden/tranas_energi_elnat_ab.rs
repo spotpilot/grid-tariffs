9b079793da9c821173928a61da4dbcebf8a5d02a
FridaSpotpilot <frida@spotpilot.io>
2025-10-02 21:11:37 +0200

    feat(SE Tranas): Add basic info (#120)
    * feat(SE Tranas): Add basic info
    
    * fix(SE Tranås): Wrong fees
    
    ---------
    
    Co-authored-by: Jacob Magnusson <m@jacobian.se>

--- src/registry/sweden/tranas_energi_elnat_ab.rs
+++ src/registry/sweden/tranas_energi_elnat_ab.rs
@@ -1,21 +1,26 @@
 use crate::registry::prelude::*;
 
-const FEE_LINK: &str = "https://tranasenergi.se/foretag/elnat/priser-och-villkor/";
+const FEE_LINK: &str = "https://tranasenergi.se/privat/elnat/priser-och-villkor/";
 
 pub static TRANAS_ENERGI_ELNAT_AB: GridOperator = GridOperator::builder()
     .name("Tranås Energi Elnät AB")
     .vat_number("SE556952020701")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(16, 35))
     .links(Links::new(
         Link::builder(FEE_LINK).content_locator_default().build(),
     ))
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(4970, 0).divide_by(12)),
+            (20, Money::new(5872, 50).divide_by(12)),
+            (25, Money::new(6996, 25).divide_by(12)),
+            (35, Money::new(9363, 75).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(33.6))
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
