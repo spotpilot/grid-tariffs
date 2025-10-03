8e21c51add3b7a20b1f6a5aae83b624a1c0cf1b4
Frida Eriksson <frida@spotpilot.io>
2025-10-03 15:15:54 +0200

    feat(SE C4 Elnat): Add basic info

--- src/registry/sweden/c_4_elnat_ab.rs
+++ src/registry/sweden/c_4_elnat_ab.rs
@@ -4,7 +4,7 @@ pub static C_4_ELNAT_AB: GridOperator = GridOperator::builder()
     .name("C4 Elnät AB")
     .vat_number("SE556496004401")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(16, 200))
     .links(
         Links::builder()
             .fee_info(
@@ -14,11 +14,23 @@ pub static C_4_ELNAT_AB: GridOperator = GridOperator::builder()
             .build(),
     )
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 7, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(6273, 0).divide_by(12)),
+            (20, Money::new(8604, 0).divide_by(12)),
+            (25, Money::new(10691, 0).divide_by(12)),
+            (35, Money::new(15540, 0).divide_by(12)),
+            (50, Money::new(22893, 0).divide_by(12)),
+            (63, Money::new(30135, 0).divide_by(12)),
+            (80, Money::new(42720, 0).divide_by(12)),
+            (100, Money::new(59750, 0).divide_by(12)),
+            (125, Money::new(84745, 0).divide_by(12)),
+            (160, Money::new(114402, 0).divide_by(12)),
+            (200, Money::new(149144, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
         .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
