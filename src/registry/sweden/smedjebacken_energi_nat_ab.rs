31a8920f9042254e21d5e2aeff97cd237cf2a2fa
Frida Eriksson <frida@spotpilot.io>
2025-10-13 15:24:01 +0200

    feat(SE Smedjebacken): Add basic info

--- src/registry/sweden/smedjebacken_energi_nat_ab.rs
+++ src/registry/sweden/smedjebacken_energi_nat_ab.rs
@@ -6,14 +6,26 @@ pub static SMEDJEBACKEN_ENERGI_NAT_AB: GridOperator = GridOperator::builder()
     .name("Smedjebacken Energi Nät AB")
     .vat_number("SE556527871901")
     .country(Country::SE)
-    .main_fuses(MainFuseSizes::new_range(16, 63))
+    .main_fuses(MainFuseSizes::new_range(16, 200))
     .links(Links::builder().fee_info(FEE_LINK, ".pagecontent").build())
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(4526, 25).divide_by(12)),
+            (20, Money::new(4526, 25).divide_by(12)),
+            (25, Money::new(6077, 50).divide_by(12)),
+            (35, Money::new(7208, 75).divide_by(12)),
+            (50, Money::new(91105, 0).divide_by(12)),
+            (63, Money::new(10352, 5).divide_by(12)),
+            (80, Money::new(11568, 75).divide_by(12)),
+            (100, Money::new(18076, 25).divide_by(12)),
+            (125, Money::new(23861, 25).divide_by(12)),
+            (160, Money::new(28200, 0).divide_by(12)),
+            (200, Money::new(38210, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .transfer_fee(TransferFee::fixed_subunit(26.25))
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
