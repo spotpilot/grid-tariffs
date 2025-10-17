80838fdc39128899de81b4aa6d10f847ed5820e0
FridaSpotpilot <frida@spotpilot.io>
2025-10-17 11:28:05 +0200

    feat(SE Ljungby): Add basic info (#156)
    * feat(SE Ljungby ): Add basic info
    
    * fix(SE Ljungby): Correct monthly fee from date
    
    * feat(SE Ljungby): Transfer fee
    
    ---------
    
    Co-authored-by: Jacob Magnusson <m@jacobian.se>

--- src/registry/sweden/ljungby_energinat_ab.rs
+++ src/registry/sweden/ljungby_energinat_ab.rs
@@ -9,11 +9,23 @@ pub static LJUNGBY_ENERGINAT_AB: GridOperator = GridOperator::builder()
     .main_fuses(MainFuseSizes::new_range(16, 63))
     .links(Links::builder().fee_info_default(FEE_LINK).build())
     .price_lists(&[PriceList::builder()
-        .from_date(9999, 12, 31)
-        .monthly_fee(Cost::Unverified)
+        .from_date(2025, 1, 1)
+        .monthly_fee(Cost::fuses(&[
+            (16, Money::new(3174, 0).divide_by(12)),
+            (20, Money::new(4564, 0).divide_by(12)),
+            (25, Money::new(5690, 0).divide_by(12)),
+            (35, Money::new(9425, 0).divide_by(12)),
+            (50, Money::new(14286, 0).divide_by(12)),
+            (63, Money::new(20538, 0).divide_by(12)),
+            (80, Money::new(28643, 0).divide_by(12)),
+            (100, Money::new(39523, 0).divide_by(12)),
+            (125, Money::new(55423, 0).divide_by(12)),
+            (160, Money::new(74714, 0).divide_by(12)),
+            (200, Money::new(104808, 0).divide_by(12)),
+        ]))
         .monthly_production_fee(Cost::Unverified)
         .feed_in_revenue(FeedInRevenue::Unverified)
-        .transfer_fee(TransferFee::Unverified)
-        .power_tariff(PowerTariff::Unverified)
+        .transfer_fee(TransferFee::spot_price_variable(8.5, 0.09, false))
+        .power_tariff(PowerTariff::NotImplemented)
         .build()])
     .build();
